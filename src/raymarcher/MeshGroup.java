14484a2766b9dc4eeaa9e75ff8bd138c309bb06e
varun unnithan <77634835+varun-un@users.noreply.github.com>
2021-06-30 23:24:10 -0400

    added mesh groups

---
+++ src/raymarcher/MeshGroup.java
@@ -0,0 +1,172 @@
+package raymarcher;
+
+import java.awt.Color;
+
+/**
+ * A class that allows for seperate meshes and geometry to be combined 
+ * into a single mesh using boolean operations, to create more complex
+ * geometry.
+ * @see <a href="https://en.wikipedia.org/wiki/Constructive_solid_geometry">Wikipedia Constructive Solid Geometery</a>
+ */
+public class MeshGroup implements Mesh{
+    
+    /** The value for a boolean union operation */
+    public static final int UNION = 0;
+    /** The value for a boolean subtraction operation */
+    public static final int SUBTRACT = 1;
+    /** The value for a boolean intersection operation */
+    public static final int INTERSECTION = 2;
+    /** The color of the mesh group */
+    private Color meshColor;
+    /** The meshgroup created for the given meshes */
+    private Mesh meshGroup;
+
+
+    /**
+     * Creates a mesh group with one of three boolean operations
+     * @param mesh1 The first mesh in for the boolean operation
+     * @param mesh2 The second mesh in for the boolean operation
+     * @param operation The operation used to create the group
+     * @param meshColor The color of the new mesh group
+     */
+    public MeshGroup(Mesh mesh1, Mesh mesh2, int operation, Color meshColor) {
+
+        if (operation == 0) {
+            meshGroup = new UnionGroup(mesh1, mesh2, meshColor);
+        }
+        if (operation == 1) {
+            meshGroup = new SubtractGroup(mesh1, mesh2, meshColor);
+        }
+        if (operation == 2) {
+            meshGroup = new IntersectionGroup(mesh1, mesh2, meshColor);
+        }
+
+        this.meshColor = meshColor;
+    }
+
+
+    /**
+     * @param meshColor the color to set this mesh group to
+     */
+    public void setMeshColor(Color meshColor) {
+        this.meshColor = meshColor;
+    }
+
+    @Override
+    public Color getMeshColor() {
+        return meshColor;
+    }
+
+    @Override
+    public double sdf(Vector3 position) {
+        return meshGroup.sdf(position);
+    }
+
+    /**
+     * An inner class representing a mesh group made with the union operation
+     */
+    public class UnionGroup implements Mesh{
+
+        /** The first mesh to be used for the operation in this mesh group */
+        private Mesh mesh1;
+        /** The second mesh to be used for the operation in this mesh group */
+        private Mesh mesh2;
+        /** The color of the mesh group */
+        private Color meshColor;
+
+        /**
+         * Creates a union between two meshes
+         * @param mesh1 The first mesh to be used for the operation in this mesh group
+         * @param mesh2 The second mesh to be used for the operation in this mesh group
+         * @param meshColor The color of the mesh group
+         */
+        public UnionGroup(Mesh mesh1, Mesh mesh2, Color meshColor) {
+            this.mesh1 = mesh1;
+            this.mesh2 = mesh2;
+            this.meshColor = meshColor;
+        }
+
+        @Override
+        public Color getMeshColor() {
+            return meshColor;
+        }
+
+        @Override
+        public double sdf(Vector3 position) {
+            
+            return (Math.min(mesh1.sdf(position), mesh2.sdf(position)));
+        }
+    }
+
+    /**
+     * An inner class representing a mesh group made with the subtract operation
+     */
+    public class SubtractGroup implements Mesh{
+
+        /** The first mesh to be used for the operation in this mesh group */
+        private Mesh mesh1;
+        /** The second mesh to be used for the operation in this mesh group */
+        private Mesh mesh2;
+        /** The color of the mesh group */
+        private Color meshColor;
+
+        /**
+         * Creates a subtraction between two meshes
+         * @param mesh1 The first mesh to be used for the operation in this mesh group
+         * @param mesh2 The second mesh to be used for the operation in this mesh group
+         * @param meshColor The color of the mesh group
+         */
+        public SubtractGroup(Mesh mesh1, Mesh mesh2, Color meshColor) {
+            this.mesh1 = mesh1;
+            this.mesh2 = mesh2;
+            this.meshColor = meshColor;
+        }
+
+        @Override
+        public Color getMeshColor() {
+            return meshColor;
+        }
+
+        @Override
+        public double sdf(Vector3 position) {
+            
+            return (Math.max(mesh1.sdf(position) * -1, mesh2.sdf(position)));
+        }
+    }
+
+    /**
+     * An inner class representing a mesh group made with the intersection operation
+     */
+    public class IntersectionGroup implements Mesh{
+
+        /** The first mesh to be used for the operation in this mesh group */
+        private Mesh mesh1;
+        /** The second mesh to be used for the operation in this mesh group */
+        private Mesh mesh2;
+        /** The color of the mesh group */
+        private Color meshColor;
+
+        /**
+         * Creates a intersection between two meshes
+         * @param mesh1 The first mesh to be used for the operation in this mesh group
+         * @param mesh2 The second mesh to be used for the operation in this mesh group
+         * @param meshColor The color of the mesh group
+         */
+        public IntersectionGroup(Mesh mesh1, Mesh mesh2, Color meshColor) {
+            this.mesh1 = mesh1;
+            this.mesh2 = mesh2;
+            this.meshColor = meshColor;
+        }
+
+        @Override
+        public Color getMeshColor() {
+            return meshColor;
+        }
+
+        @Override
+        public double sdf(Vector3 position) {
+            
+            return (Math.max(mesh1.sdf(position), mesh2.sdf(position)));
+        }
+    }
+}
--- src/raymarcher/TestScene.java
+++ src/raymarcher/TestScene.java
@@ -12,16 +12,20 @@ public class TestScene {
     public static void main(String[] args) {
 
         Scene myScene = new Scene();
-        Box box = new Box(new Vector3(0,0,0), new Vector3(1, 1, 1), Color.BLUE);
+        // Box box = new Box(new Vector3(0,0,0), new Vector3(1, 1, 1), Color.BLUE);
         // myScene.add(box);
-        // myScene.add(new Sphere(new Vector3(0,0,0), 1, Color.BLACK));
+        Sphere sphere = new Sphere(new Vector3(0,0,0), 1, Color.BLACK);
 
-
-        Torus torus = new Torus(new Vector3(), 2, 1, Color.RED);
+        
+        
+        Torus torus = new Torus(new Vector3(), 1, .5, Color.RED);
         // myScene.add(torus);
-
+        
         TriangularPrism triangularPrism = new TriangularPrism(new Vector3(), 1, 1, Color.WHITE);
-        myScene.add(triangularPrism);
+        // myScene.add(triangularPrism);
+        
+        myScene.add(new MeshGroup(torus, sphere, MeshGroup.SUBTRACT, Color.RED));
+
 
         Camera camera = new Camera(new Vector3(0,0,5), new Vector3(0,0,-1), new Vector3(0, 1, 0), .1, myScene);
 
